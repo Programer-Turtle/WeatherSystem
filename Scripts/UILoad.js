702477e02b3c422ab1d9fb311a1c42bee9810c1f
Informare <karsonulerick@gmail.com>
2024-08-15 17:10:58 -0400

    Update

---
+++ Scripts/UILoad.js
@@ -0,0 +1,27 @@
+async function LoadNavBar()
+{
+    try 
+    {
+        document.getElementById("NavBar").innerHTML = `<a style="padding: 0; margin: 0; margin-top: 5px; margin-left: 10px; margin-right: 40px;"href="index.html"><img style="padding: 0; margin: 0;" width="80px" src="Images/Logo.PNG" alt="Logo"></a>`
+        return true    
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+
+async function LoadUI()
+{
+    StatusChecks = []
+    StatusChecks.push(await LoadNavBar())
+    if (!StatusChecks.includes(false))
+    {
+        console.log("No errors occured when loading UI.")
+    }
+    else
+    {
+        console.log("Errors occured when loading UI.")
+    }
+}
+
+LoadUI()
