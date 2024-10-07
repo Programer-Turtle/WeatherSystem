b34f43fe72c0a92537819e7dad389d47991c5408
Informare <karsonulerick@gmail.com>
2024-10-06 23:03:26 -0400

    Added Henry

--- Scripts/Counties.js
+++ Scripts/Counties.js
@@ -1,3 +1,5 @@
+//https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJsb2NhbCIsImNlbnRlciI6Wy04Ni4yOCwzOS43MDddLCJsb2NhdGlvbiI6bnVsbCwiem9vbSI6OC40OTU2NDQ5MzIwNDQ0MzUsImZpbHRlciI6bnVsbCwibGF5ZXIiOiJzcl9icmVmIiwic3RhdGlvbiI6IktJTkQifSwiYW5pbWF0aW5nIjp0cnVlLCJiYXNlIjoic3RhbmRhcmQiLCJhcnRjYyI6ZmFsc2UsImNvdW50eSI6ZmFsc2UsImN3YSI6ZmFsc2UsInJmYyI6ZmFsc2UsInN0YXRlIjpmYWxzZSwibWVudSI6dHJ1ZSwic2hvcnRGdXNlZE9ubHkiOnRydWUsIm9wYWNpdHkiOnsiYWxlcnRzIjowLjY5LCJsb2NhbCI6MSwibG9jYWxTdGF0aW9ucyI6MCwibmF0aW9uYWwiOjAuNn19
+
 Data = {
     'Indiana':{
         'Wayne':{
@@ -7,6 +9,14 @@ Data = {
             'Longitude':'-84.99676590292087',
             'ReflectivityRadar':'https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJsb2NhbCIsImNlbnRlciI6Wy04My44MjIsMzkuNDJdLCJsb2NhdGlvbiI6bnVsbCwiem9vbSI6OC40OTU2NDQ5MzIwNDQ0MzUsImZpbHRlciI6bnVsbCwibGF5ZXIiOiJzcl9icmVmIiwic3RhdGlvbiI6IktJTE4ifSwiYW5pbWF0aW5nIjp0cnVlLCJiYXNlIjoic3RhbmRhcmQiLCJhcnRjYyI6ZmFsc2UsImNvdW50eSI6ZmFsc2UsImN3YSI6ZmFsc2UsInJmYyI6ZmFsc2UsInN0YXRlIjpmYWxzZSwibWVudSI6dHJ1ZSwic2hvcnRGdXNlZE9ubHkiOnRydWUsIm9wYWNpdHkiOnsiYWxlcnRzIjowLjY5LCJsb2NhbCI6MSwibG9jYWxTdGF0aW9ucyI6MCwibmF0aW9uYWwiOjAuNn19',
             'VelocityRadar':'https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJsb2NhbCIsImNlbnRlciI6Wy04My45NjQsMzkuNDk2XSwibG9jYXRpb24iOm51bGwsInpvb20iOjguNDk1NjQ0OTMyMDQ0NDM1LCJmaWx0ZXIiOm51bGwsImxheWVyIjoic3JfYnZlbCIsInN0YXRpb24iOiJLSUxOIn0sImFuaW1hdGluZyI6dHJ1ZSwiYmFzZSI6InN0YW5kYXJkIiwiYXJ0Y2MiOmZhbHNlLCJjb3VudHkiOmZhbHNlLCJjd2EiOmZhbHNlLCJyZmMiOmZhbHNlLCJzdGF0ZSI6ZmFsc2UsIm1lbnUiOnRydWUsInNob3J0RnVzZWRPbmx5Ijp0cnVlLCJvcGFjaXR5Ijp7ImFsZXJ0cyI6MC42OSwibG9jYWwiOjEsImxvY2FsU3RhdGlvbnMiOjAsIm5hdGlvbmFsIjowLjZ9fQ%3D%3D'
+        },
+        'Henry':{
+            'CountyCode':'INC065',
+            'ForecastCode':'INZ049',
+            'Latitude':'39.9274',
+            'Longitude':'-85.3719',
+            'ReflectivityRadar':'https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJsb2NhbCIsImNlbnRlciI6Wy04Ni4yOCwzOS43MDddLCJsb2NhdGlvbiI6bnVsbCwiem9vbSI6OC40OTU2NDQ5MzIwNDQ0MzUsImZpbHRlciI6bnVsbCwibGF5ZXIiOiJzcl9icmVmIiwic3RhdGlvbiI6IktJTkQifSwiYW5pbWF0aW5nIjp0cnVlLCJiYXNlIjoic3RhbmRhcmQiLCJhcnRjYyI6ZmFsc2UsImNvdW50eSI6ZmFsc2UsImN3YSI6ZmFsc2UsInJmYyI6ZmFsc2UsInN0YXRlIjpmYWxzZSwibWVudSI6dHJ1ZSwic2hvcnRGdXNlZE9ubHkiOnRydWUsIm9wYWNpdHkiOnsiYWxlcnRzIjowLjY5LCJsb2NhbCI6MSwibG9jYWxTdGF0aW9ucyI6MCwibmF0aW9uYWwiOjAuNn19',
+            'VelocityRadar':'https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJsb2NhbCIsImNlbnRlciI6Wy04Ni4yOCwzOS43MDddLCJsb2NhdGlvbiI6bnVsbCwiem9vbSI6OC40OTU2NDQ5MzIwNDQ0MzUsImZpbHRlciI6bnVsbCwibGF5ZXIiOiJzcl9idmVsIiwic3RhdGlvbiI6IktJTkQifSwiYW5pbWF0aW5nIjp0cnVlLCJiYXNlIjoic3RhbmRhcmQiLCJhcnRjYyI6ZmFsc2UsImNvdW50eSI6ZmFsc2UsImN3YSI6ZmFsc2UsInJmYyI6ZmFsc2UsInN0YXRlIjpmYWxzZSwibWVudSI6dHJ1ZSwic2hvcnRGdXNlZE9ubHkiOnRydWUsIm9wYWNpdHkiOnsiYWxlcnRzIjowLjY5LCJsb2NhbCI6MSwibG9jYWxTdGF0aW9ucyI6MCwibmF0aW9uYWwiOjAuNn19'
         }
     }
 }
