5221dcf6d77637e634269f14f303f1bfdb8f3caa
RAAAAAAAAAAAAAAAAATZ <karsonulerick@gmail.com>
2024-10-16 21:58:49 -0400

    Added Alerts

--- Scripts/Weather.js
+++ Scripts/Weather.js
@@ -93,16 +93,16 @@ async function MainForecast() {
                     AlertHeadline=`${currentAlert['properties']['event']}\n`
                 }
     
-                AlertText += `${currentAlert['properties']['headline']}\n\nSeverity\n${currentAlert['properties']['severity']}`
+                AlertText += `<span style='font-weight: bold;'>${currentAlert['properties']['headline']}</span>\n\n<span style='font-weight: bold;'>Severity</span>\n${currentAlert['properties']['severity']}`
 
                 if(! ["", "null", null].includes(currentAlert['properties']['certainty'])){
-                    AlertText+=`\n\nCertainty\n${currentAlert['properties']['certainty']}`
+                    AlertText+=`\n\n<span style='font-weight: bold;'>Certainty</span>\n${currentAlert['properties']['certainty']}`
                 }
 
-                AlertText+=`\n\nDescription\n${currentAlert['properties']['description']}`
+                AlertText+=`\n\n<span style='font-weight: bold;'>Description</span>\n${currentAlert['properties']['description']}`
                 
                 if (! ["", "null", null].includes(currentAlert['properties']['instruction'])){
-                    AlertText+=`\n\nInstruction\n${currentAlert['properties']['instruction']}`
+                    AlertText+=`\n\n<span style='font-weight: bold;'>Instruction</span>\n${currentAlert['properties']['instruction']}`
                 }
     
                 //Creat Text Box
@@ -113,8 +113,9 @@ async function MainForecast() {
                 AlertHeadlineHeader.innerText = AlertHeadline
                 AlertHeadlineHeader.style = "text-align: left;font-size: 25px;"
 
+                AlertText = AlertText.replaceAll("\n", "<br>")
                 let AlertTextParagraph = document.createElement("p")
-                AlertTextParagraph.innerText = AlertText
+                AlertTextParagraph.innerHTML = AlertText
 
                 AlertBox.appendChild(AlertHeadlineHeader)
                 AlertBox.appendChild(AlertTextParagraph)
@@ -150,5 +151,105 @@ async function MainForecast() {
 
         AlertBox.appendChild(AlertTextParagraph)
         AlertList.appendChild(AlertBox)
+    }
+}
+
+async function GetActiveAlert(AlertID) {
+    console.log("Getting Alerts")
+    let AlertsRequest = await fetch('https://api.weather.gov/alerts/active/', {
+        method:'Get'
+    })
+    if(!AlertsRequest.ok){
+        location.reload()
+    }
+    console.log(AlertsRequest)
+
+    let Alerts = await AlertsRequest.json()
+    let AlertsList = Alerts['features']
+    
+    for(let i = 0; i<AlertsList.length; i++){
+        if(AlertsList[i]['properties']['id'] == AlertID){
+            console.log(AlertsList[i])
+            return AlertsList[i]
+        }
+    }
+    return false
+    
+}
+
+async function GetPastAlerts(AlertID) {
+    console.log("Getting Alerts")
+    let AlertsRequest = await fetch('https://api.weather.gov/alerts/', {
+        method:'Get'
+    })
+    if(!AlertsRequest.ok){
+        location.reload()
+    }
+    console.log(AlertsRequest)
+
+    let Alerts = await AlertsRequest.json()
+    let AlertsList = Alerts['features']
+    
+    for(let i = 0; i<AlertsList.length; i++){
+        if(AlertsList[i]['properties']['id'] == AlertID){
+            console.log(AlertsList[i])
+            return AlertsList[i]
+        }
+    }
+    return false
+}
+
+async function AlertLoad() {
+    let link = location.href
+    link = link.replaceAll("%3A", ":")
+    console.log(link)
+    let AlertText = ""
+    let Header = document.getElementById("AlertHeader")
+    let AlertBox = document.getElementById("AlertBox")
+    if(link.includes('?')){
+        let AlertID = link.split("?")[1]
+        let AlertData = await GetActiveAlert(AlertID)
+        if(AlertData == false){
+            AlertData = await GetPastAlerts(AlertID)
+            AlertText+="<span style='font-weight: bold;'>Expired</span>\n"
+        }
+        if(!AlertData==false){
+            if(!["", "null", null].includes(AlertData['properties']['parameters']['NWSheadline'])){
+                Header.innerText=`${AlertData['properties']['parameters']['NWSheadline'][0]}\n`
+            }
+            else{
+                Header.innerText=`${AlertData['properties']['event']}\n`
+            }
+
+            AlertText += `<span style='font-weight: bold;'>${AlertData['properties']['headline']}</span>\n\n<span style='font-weight: bold;'>Severity</span>\n${AlertData['properties']['severity']}`
+
+            if(! ["", "null", null].includes(AlertData['properties']['certainty'])){
+                AlertText+=`\n\n<span style='font-weight: bold;'>Certainty</span>\n${AlertData['properties']['certainty']}`
+            }
+
+            AlertText+=`\n\n<span style='font-weight: bold;'>Description</span>\n${AlertData['properties']['description']}`
+                
+            if (! ["", "null", null].includes(AlertData['properties']['instruction'])){
+                AlertText+=`\n\n<span style='font-weight: bold;'>Instructions</span>\n${AlertData['properties']['instruction']}`
+            }
+
+            //Adds to HTML
+            let AlertTextArea = document.createElement('p')
+            AlertText = AlertText.replaceAll("\n", "<br>")
+            AlertTextArea.innerHTML = AlertText
+            AlertBox.appendChild(AlertTextArea)
+        }
+        else{
+            Header.innerText = "Invalid Alert URL"
+            let ErrorTextArea = document.createElement('p')
+            ErrorTextArea.innerHTML = "<h1>This alert doesn't exist or was deleted.</h1>"
+            AlertBox.appendChild(ErrorTextArea)
+        }
+    }
+    else{
+        Header.innerText = "No Alert Entered"
+        let ErrorTextArea = document.createElement('p')
+        ErrorTextArea.innerHTML = "<h1>Please use an official URL.</h1>"
+        AlertBox.appendChild(ErrorTextArea)
     }
 }
