78b4da00b3841833b11fba4506999e04c6921a48
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2025-04-02 16:04:44 -0400

    Update Search.js

--- Scripts/Search.js
+++ Scripts/Search.js
@@ -34,7 +34,7 @@ function Search(){
 }
 
 async function SetLocation(State, County) {
-    let Response = await fetch('https://weather.informapi.xyz/setLocation', {
+    let Response = await fetch('https://api.weathersystem.org/setLocation', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
